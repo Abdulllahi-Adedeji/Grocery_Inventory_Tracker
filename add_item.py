c9d3192e73949361b65565b48d2531b285254cf2
Abdullahi Adedeji <abdullahikolawole.adedeji@student.griffith.ie>
2026-02-05 15:06:09 +0000

    adding check for inserting duplicate items

--- add_item.py
+++ add_item.py
@@ -3,17 +3,38 @@ from db_Connector import get_connection
 def add_item(name, quantity, price, category=None, expiry_date=None, low_stock_min=5):
     conn = get_connection()
     cursor = conn.cursor()
+
+    try:
+
+        cursor.execute("SELECT item_id, quantity FROM items WHERE name = %s", (name,))
+        existing = cursor.fetchone()
+
+        if existing:
     
-    sql = """
-    INSERT INTO items (name, quantity, price, category, expiry_date, low_stock_min)
-    VALUES (%s, %s, %s, %s, %s, %s)
-    """
-    cursor.execute(sql, (name, quantity, price, category, expiry_date, low_stock_min))
-    conn.commit()
-    
-    # Low stock check
-    if quantity <= low_stock_min:
-        print(f" {name} is low on stock!")
-    
-    cursor.close()
-    conn.close()
+            new_qty = existing[1] + quantity
+            cursor.execute("UPDATE items SET quantity = %s WHERE item_id = %s", (new_qty, existing[0]))
+            conn.commit()
+            print(f" Updated quantity of {name} to {new_qty}")
+
+ 
+            if new_qty <= low_stock_min:
+                print(f"⚠️ {name} is low on stock!")
+            return
+
+        sql = """
+        INSERT INTO items (name, quantity, price, category, expiry_date, low_stock_min)
+        VALUES (%s, %s, %s, %s, %s, %s)
+        """
+        cursor.execute(sql, (name, quantity, price, category, expiry_date, low_stock_min))
+        conn.commit()
+
+
+        if quantity <= low_stock_min:
+            print(f"⚠️ {name} is low on stock!")
+
+    except Exception as e:
+        print("Error:", e)
+
+    finally:
+        cursor.close()
+        conn.close()
