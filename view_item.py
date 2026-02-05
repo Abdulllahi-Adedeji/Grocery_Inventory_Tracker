905ebcae1069a42facd551171533b90aa94a2b17
Abdullahi Adedeji <abdullahikolawole.adedeji@student.griffith.ie>
2026-02-05 16:01:21 +0000

    adding search feature with view_items

--- view_item.py
+++ view_item.py
@@ -1,15 +1,14 @@
 from db_Connector import get_connection
 from tabulate import tabulate
 
-def view_items(category=None, low_stock_only=False):
+def view_items(category=None, low_stock_only=False, search=None):
     conn = get_connection()
     cursor = conn.cursor()
-
     try:
         sql = "SELECT item_id, name, quantity, price, category, expiry_date, low_stock_min FROM items"
         filters = []
         values = []
-        
+
         if category:
             filters.append("category = %s")
             values.append(category)
@@ -17,11 +16,14 @@ def view_items(category=None, low_stock_only=False):
         if low_stock_only:
             filters.append("quantity <= low_stock_min")
 
+        if search:
+            filters.append("name LIKE %s")
+            values.append(f"%{search}%")
+
         if filters:
             sql += " WHERE " + " AND ".join(filters)
 
         sql += " ORDER BY name"
-
         cursor.execute(sql, tuple(values))
         rows = cursor.fetchall()
 
@@ -30,12 +32,10 @@ def view_items(category=None, low_stock_only=False):
             return
 
         headers = ["ID", "Name", "Qty", "Price", "Category", "Expiry", "LowStockMin"]
-
         print(tabulate(rows, headers=headers, tablefmt="pretty"))
 
     except Exception as e:
         print(" Failed to fetch items:", e)
-
     finally:
         cursor.close()
         conn.close()
